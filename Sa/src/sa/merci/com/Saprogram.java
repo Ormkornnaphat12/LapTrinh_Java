cd41ee36b8331d101a11605543970f2104cd8652
Ormkornnaphat12 <thu0989905368@gmail.com>
2026-04-18 10:38:33 +0700

    Thuc hanh Scanner Nhap so nguyen

---
+++ Sa/src/sa/merci/com/Saprogram.java
@@ -0,0 +1,14 @@
+package sa.merci.com;
+import java.util.Scanner;
+
+public class Saprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Nhap so nguyen: ");
+		int a=sc.nextInt();
+
+	}
+
+}
